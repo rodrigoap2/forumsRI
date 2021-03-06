ec3e82d60280c68e20000c8adbdace248626ea94
rodrigoap2 <rap2@cin.ufpe.br>
2021-03-06 00:05:03 -0300

    Create require_sitemaps function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
+import time
+from bs4 import BeautifulSoup
 
-links = ["https://comunidade.nubank.com.br/",
-        "https://community.vtex.com/",
-        "https://comunidade.nuvemshop.com.br/",
+links = [
         "https://community.shopify.com/",
-        "https://discussions.apple.com/",
         "https://community.spotify.com/",
         "https://community.acer.com/"
         ]
@@ -18,25 +17,52 @@ def parse_robots_txt(html):
     actual_user_agent = ""
     for line in lines:
         if ":" in line:
-            line_infos = line.split(":")
+            line_infos = line.split(": ")
             if "sitemap" in line_infos[0].lower():
-                informations["Sitemap"] = "".join(line_infos[1:]).split(" ")[1] #Getting the sitemap
-                print(informations["Sitemap"])
+                informations["Sitemap"] = "".join(line_infos[1:]).split(" ")[0] #Getting the sitemap
             elif "user-agent" in line_infos[0].lower():
-                actual_user_agent = "".join(line_infos[1:]).split(" ")[1]
+                actual_user_agent = "".join(line_infos[1:]).split(" ")[0]
             elif "disallow" in line_infos[0].lower():
                 if(actual_user_agent == "*"):
-                    informations["Disallow"].append("".join(line_infos[1:]).split(" ")[1])
+                    informations["Disallow"].append("".join(line_infos[1:]).split(" ")[0])
             elif "crawl-delay" in line_infos[0].lower():
                 if(actual_user_agent == "*"):
-                    informations["Crawl-delay"] = int("".join(line_infos[1:]).split(" ")[1])
+                    informations["Crawl-delay"] = int("".join(line_infos[1:]).split(" ")[0])
             elif "allow" in line_infos[0].lower():
                 if(actual_user_agent == "*"):
-                    informations["Allow"].append("".join(line_infos[1:]).split(" ")[1])
+                    informations["Allow"].append("".join(line_infos[1:]).split(" ")[0])
     return informations
+
+def require_sitemaps(sitemap, delay):
+    if delay > 0:
+        time.sleep(delay)
+    sitemaps_dict = {}
+
+    request = requests.get(sitemap)
+    xml = request.text
+
+    soup = BeautifulSoup(xml, "html.parser")
+    sitemap_tags = soup.find_all("sitemap")
+
+    while sitemap_tags != [] and not(sitemap_tags[0].findNext("loc").text in sitemaps_dict):
+        site = sitemap_tags[0].findNext("loc").text
+        sitemaps_dict[site] = sitemap_tags[0].findNext("lastmod").text
+        if delay > 0:
+            time.sleep(delay)
+    
+        request = requests.get(site)
+        xml = request.text
+        soup = BeautifulSoup(xml, "html.parser")
+        sitemaps = soup.find_all("sitemap")
+        sitemap_tags.pop(0)
+        for element in sitemaps:
+            sitemap_tags.append(element)
+        
+    return sitemaps_dict
 
 def main():
     for link in links:
         robot_informations = parse_robots_txt(get_robots_txt(link))
+        sitemaps = require_sitemaps(robot_informations["Sitemap"], robot_informations["Crawl-delay"])
 
 main()
