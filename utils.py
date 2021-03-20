5de1f4a702755d79a4cf2a03f3557609074872db
Leao Liu <llm2@cin.ufpe.br>
2021-03-20 14:17:32 -0300

    Add generate_csv.py and modify utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 from unidecode import unidecode
 
+def get_parsed_phrase(soup):
+    title = preprocess_phrase(soup.find('title').text)
+    body = get_parsed_body(soup)
+    return title + " " + max(body[1:], key=lambda s: len(s))
+
 def get_parsed_body(soup):
-    # return [s.strip() for s in soup.find('body').text.split('\n') if s.strip() != '']
     return [preprocess_phrase(s) for s in soup.find('body').text.split('\n') if s.strip() != '']
 
 def preprocess_phrase(phrase):
