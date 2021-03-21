6f916254ebee4314098798f53e7ae946692b5396
rodrigoap2 <rap2@cin.ufpe.br>
2021-03-20 23:39:09 -0300

    Update bfs crawler to multithreading

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ import validators
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
 links = [
+    "https://comunidade.nubank.com.br",
+    "https://comunidade.nuvemshop.com.br",
+    "https://community.shopify.com/",
     "https://community.vtex.com"
 ]
 
 threadLock = threading.Lock()
 global_counter = 0
 rp = urllib.robotparser.RobotFileParser()
+visited_links = []
 
 def get_robots_txt(url):
     return requests.get(f"{url}/robots.txt").text
@@ -43,7 +48,8 @@ def parse_robots_txt(html):
                     informations["Allow"].append("".join(line_infos[1:]).split(" ")[0])
     return informations
 
-def validate_link(link, base_link, rp):
+def validate_link(link, base_link):
+    global rp
     if link != None and link:
         if 'javascript:void(0);' in link or 'None' in link or not rp.can_fetch('*', link):
             return False
@@ -52,50 +58,70 @@ def validate_link(link, base_link, rp):
     else:
         return False
 
-def get_site_content(link, delay):
-    global global_counter
+def get_site_content(link, base_link, delay):
+    global global_counter, visited_links
     if delay != None:
         time.sleep(delay)
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--incognito")
+    options.headless = True
+    options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=options)
     driver.get(link)
-    time.sleep(2)
+    time.sleep(5)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     with threadLock:
         global_counter += 1
+        with open("links2.txt", "a") as myfile:
+            myfile.write(link + '\n')
+        print(global_counter)
     result = soup.findAll('a')
     driver.close()
-    return result
-
-def links_bfs(base_link, rp):
-    global global_counter
-    delay = rp.crawl_delay("*")
-    a_tags = get_site_content(base_link, delay)
     links = []
-    visited_links = []
-    visited_links.append(base_link)
-    for link in a_tags:
+    for link in result:
         link = link.get('href')
         if not 'http' in str(link):
             link = base_link + str(link)
-        if validate_link(link, base_link, rp) and link not in visited_links:
+        if validate_link(link, base_link) and link not in visited_links and validators.url(link):
             links.append(link)
+    return links
+
+def links_bfs(base_link, rp):
+    global global_counter, visited_links
+    delay = rp.crawl_delay("*")
+    links = get_site_content(base_link, base_link, delay)
+    visited_links.append(base_link)
     while links != [] and global_counter < 1000:
         print(global_counter)
-        if validators.url(links[0]) and not links[0] in visited_links:
-            actual_links = get_site_content(links[0], delay)
-            visited_links.append(links[0])
-            links.pop(0)
-            for link in actual_links:
-                link = link.get('href')
-                if not 'http' in str(link):
-                    link = base_link + str(link)
-                if validate_link(link, base_link, rp) and link not in visited_links:
-                    links.append(link)
-        else: 
-            links.pop(0)
-
+        links_results = set_up_threads(links, base_link, delay)
+        for link in links:
+            visited_links.append(link)
+        links = []
+        counter = 0
+        for link_array in links_results:
+            for link in link_array:
+                if counter + global_counter >= 1000:
+                    break
+                else:
+                    if not link in visited_links:
+                        links.append(link)
+                        counter += 1
+        
     return visited_links
-    
+
+def set_up_threads(links, base_link, delay):
+    base_links = []
+    delays = []
+    for link in links:
+         base_links.append(base_link)
+         delays.append(delay)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        return executor.map(get_site_content,  
+                            links,
+                            base_links,
+                            delays,
+                            timeout = 30)
 
 def main():
     for link in links:
@@ -105,7 +131,6 @@ def main():
         #sitemaps = require_sitemaps(robot_informations["Sitemap"], robot_informations["Crawl-delay"])
         # for sitemap in sitemaps:
         #     get_sitemap_links(sitemap[0], link_counter)
-        link_counter = 0
         site_links = links_bfs(link, rp)
         print(site_links)
 main()
